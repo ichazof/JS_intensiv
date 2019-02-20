3713f70f5b51b72591faa0db6e768368415f00b4
Iya <ichazof@ya.ru>
2019-02-20 12:43:13 +0500

    second lesson

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ window.addEventListener('DOMContentLoaded', () => {
         goodsBtn = document.querySelectorAll('.goods__btn'),
         products = document.querySelectorAll('.goods__item'),
         confirm = document.querySelector('.confirm'),
-        badge = document.querySelector('.nav__bage'),
+        badge = document.querySelector('.nav__badge'),
         totalCost = document.querySelector('.cart__total > span'),
-        titles = document.querySelector('.goods__title');
+        titles = document.querySelectorAll('.goods__title');
     function openCart() {
         cart.style.display = 'block';
         document.body.style.overflow = 'hidden';
@@ -29,13 +29,79 @@ window.addEventListener('DOMContentLoaded', () => {
                 removeBtn = document.createElement('div'),
                 empty = cartWrapper.querySelector('.empty');
             trigger.remove();
+            showConfirm();
+            calcGoods(1);
             removeBtn.classList.add('goods__item-remove');
             removeBtn.innerHTML = '&times';
             item.appendChild(removeBtn);
             cartWrapper.appendChild(item);
+            
             if (empty) {
                 empty.remove();
-            }            
+            }
+            calcTotal();  
+            deleteFromCart();          
         });
     });
+    function sliceTitle() {
+        titles.forEach(function(item) {
+            if (item.textContent.length > 70) {
+                item.textContent = `${item.textContent.slice(0,70)}...`;
+            }
+        });
+    }
+    sliceTitle();
+
+    function showConfirm() {
+        confirm.style.display = 'block';
+        let counter = 100;
+        const id = setInterval(frame, 10); 
+        function frame() {
+            if (counter == 10) {
+                clearInterval(id);
+                confirm.style.display = 'none';
+            } else {
+                counter--;
+                confirm.style.transform = `translateY(-${counter}px)`;
+                confirm.style.opacity = '.' + counter;
+
+            }
+            
+        }
+    }
+
+    function calcGoods(i) {
+        const items = cartWrapper.querySelectorAll('.goods__item');
+        badge.textContent = i + items.length;
+
+    }
+
+    function calcTotal() {
+        const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+        let total = 0;
+
+        prices.forEach(function(item) {
+            total += +item.textContent;
+
+        });
+        totalCost.textContent = total;
+    }
+
+    function deleteFromCart() {
+        const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+        removeBtn.forEach(function(btn) {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                calcGoods(0);
+                calcTotal();
+                if (cartWrapper.querySelectorAll('.goods__item').length == 0) {
+                    const emptyText = document.createElement('div');
+                    emptyText.classList.add('empty');
+                    //empty.querySelector('.empty').textContent = 'Ваша корзина пуста';
+                    cartWrapper.appendChild(emptyText);
+                    cartWrapper.querySelector('.empty').textContent = 'Ваша корзина пуста';
+                };
+            });
+        });
+    }
 });
